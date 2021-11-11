e6a35b38963b7257973ffbfd01b2748e97391794
cnegreanu <calin.negreanu@8x8.com>
2021-11-11 16:27:54 +0200

    - Update

--- src/MultidocServiceProvider.php
+++ src/MultidocServiceProvider.php
@@ -11,7 +11,7 @@ class MultidocServiceProvider extends ServiceProvider
 {
 
     protected $commands = [
-        'Commands\GenerateDocumentationCommand',
+        'Multidoc\Commands\GenerateDocumentationCommand',
     ];
 
     /**
